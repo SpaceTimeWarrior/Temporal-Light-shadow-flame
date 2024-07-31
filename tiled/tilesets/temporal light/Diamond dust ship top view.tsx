0e44c3c6e5110a62496006e12820e4f10d7c2f5d
SpaceTimeWarrior <stevenjr@comporium.net>
2024-07-31 11:30:15 -0400

    final gamejam update

---
+++ tiled/tilesets/temporal light/Diamond dust ship top view.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Diamond dust starship top view" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <image source="lf_normal/2139f486-32d0-4e16-8c1f-cf27800c34d0.png" width="64" height="64"/>
+</tileset>
