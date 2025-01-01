5753f21b254bdfa03313efb772c1debc2fe47c1f
SpaceTimeWarrior <stevenjr@comporium.net>
2024-12-31 23:38:34 -0500

    New year update

--- tiled/tilesets/kenny modified/full image.tsx
+++ tiled/tilesets/kenny modified/full image.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="full image" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="full image.png" width="1024" height="1024"/>
+<tileset version="1.10" tiledversion="1.11.0" name="full image" tilewidth="16" tileheight="16" tilecount="8192" columns="128">
+ <image source="full image.png" width="2048" height="1024"/>
 </tileset>
